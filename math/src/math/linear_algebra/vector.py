3528528096dc7ecadb8f88763c2abc79a919398f
behike56 <fxy.x@lambda-publishing.com>
2026-05-24 12:51:15 +0900

    Add.

--- math/src/math/linear_algebra/vector.py
+++ math/src/math/linear_algebra/vector.py
@@ -1,20 +1,9 @@
 import numpy as np
 
+u = np.array([1.0, 2.0, 3.0])
+v = np.array([4.0, 5.0, 6.0])
 
-a = np.array([1, 2, 3, 4])
-b = np.array([10, 20, 30, 40])
+print(u + v)
+print(u - v)
 
-c = a + b
-print(c)  # [11 22 33 44]
-
-
-vectors = np.array(
-    [
-        [1, 2, 3, 4],
-        [10, 20, 30, 40],
-        [-1, 0, 5, 2],
-    ]
-)
-
-s = vectors.sum(axis=0)
-print(s)
+print()
